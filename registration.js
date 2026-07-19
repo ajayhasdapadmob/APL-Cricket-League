5a7c6994c77874a41855a4961ccf65c1c708374d
ajayhasdapadmob <ajayhasda623@gmail.com>
2026-07-19 22:06:28 +0530

    update registration.js

--- registration.js
+++ registration.js
@@ -1,4 +1,16 @@
-.catch((error)=>{
+signInWithPhoneNumber(
+    auth,
+    phoneNumber,
+    window.recaptchaVerifier
+)
+.then((result) => {
+    confirmationResult = result;
+
+    alert("OTP Sent Successfully");
+
+    startTimer();
+})
+.catch((error) => {
     console.log(error);
     alert("Error Code: " + error.code + "\n\n" + error.message);
 });
