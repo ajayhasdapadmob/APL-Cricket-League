85f7e2de8a825e05512de3e956e4956d713b5c1c
ajayhasdapadmob <ajayhasda623@gmail.com>
2026-07-19 20:09:59 +0530

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import {
-  getAuth,
-  RecaptchaVerifier,
-  signInWithPhoneNumber
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC5sHNe2gV3vJYA1F2-JQsNbZrU5DTLcPw",
   authDomain: "ajay-premier-league-apl.firebaseapp.com",
   projectId: "ajay-premier-league-apl",
   storageBucket: "ajay-premier-league-apl.firebasestorage.app",
   messagingSenderId: "514698419085",
-  appId: "1:514698419085:web:
+  appId: "1:514698419085:web:30264cdf0ee9629706bbc4",
+  measurementId: "G-8VMTN4PPQZ"
+};
