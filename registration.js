8224f00a276454349a4cca6b20c008b91acd4014
ajayhasdapadmob <ajayhasda623@gmail.com>
2026-07-19 23:35:49 +0530

    update registration.js

--- registration.js
+++ registration.js
@@ -1,4 +1,47 @@
+import { auth } from "./firebase.js";
+
 import { 
   signInWithPhoneNumber,
   RecaptchaVerifier 
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+let confirmationResult = null;
+
+
+window.sendOTP = function () {
+
+    alert("Send OTP button clicked");
+
+    let mobile = document.getElementById("mobile").value;
+
+    if (mobile.length !== 10) {
+        alert("Enter valid 10 digit mobile number");
+        return;
+    }
+
+    let phoneNumber = "+91" + mobile;
+
+    window.recaptchaVerifier = new RecaptchaVerifier(
+        auth,
+        "recaptcha-container",
+        {
+            size: "normal"
+        }
+    );
+
+    signInWithPhoneNumber(
+        auth,
+        phoneNumber,
+        window.recaptchaVerifier
+    )
+    .then((result)=>{
+        confirmationResult = result;
+        alert("OTP Sent Successfully");
+    })
+    .catch((error)=>{
+        alert("Error Code: " + error.code);
+        console.log(error);
+    });
+
+};
