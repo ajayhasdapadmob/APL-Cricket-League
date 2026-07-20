edd9cccd6cfe57c2e688f5a47fe00efcba4e9f71
ajayhasdapadmob <ajayhasda623@gmail.com>
2026-07-20 22:33:06 +0530

    update

--- registration.js
+++ registration.js
@@ -1,54 +1,10 @@
-import { auth, db } from "./firebase.js";
-
-import {
-  signInWithPhoneNumber,
-  RecaptchaVerifier
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { db } from "./firebase.js";
 
 import {
   collection,
   addDoc,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-let confirmationResult = null;
-
-window.sendOTP = function () {
-
-    alert("Send OTP button clicked");
-
-    let mobile = document.getElementById("mobile").value;
-
-    if (mobile.length !== 10) {
-        alert("Enter valid 10 digit mobile number");
-        return;
-    }
-
-    let phoneNumber = "+91" + mobile;
-
-    window.recaptchaVerifier = new RecaptchaVerifier(
-        auth,
-        "recaptcha-container",
-        {
-            size: "normal"
-        }
-    );
-
-    signInWithPhoneNumber(
-        auth,
-        phoneNumber,
-        window.recaptchaVerifier
-    )
-    .then((result)=>{
-        confirmationResult = result;
-        alert("OTP Sent Successfully");
-    })
-    .catch((error)=>{
-        alert("Error Code: " + error.code);
-        console.log(error);
-    });
-
-};
 document.getElementById("registrationForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
@@ -61,6 +17,9 @@ document.getElementById("registrationForm").addEventListener("submit", async (e)
             email: document.getElementById("email").value,
             area: document.getElementById("area").value,
             upi: document.getElementById("upi").value,
+            address: document.getElementById("address").value,
+            
+playerType: document.getElementById("playerType").value,
             status: "Pending",
             createdAt: serverTimestamp()
         });
