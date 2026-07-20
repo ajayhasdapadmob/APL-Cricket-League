eb7b4939d8f9714e8a0027682e247c384da8c5ad
ajayhasdapadmob <ajayhasda623@gmail.com>
2026-07-20 22:56:25 +0530

    update admin.js

--- admin.js
+++ admin.js
@@ -2,18 +2,22 @@ import { db } from "./firebase.js";
 
 import {
   collection,
-  getDocs
+  getDocs,
+  doc,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const table = document.getElementById("registrationTable");
 
 async function loadRegistrations() {
+
     table.innerHTML = "";
 
     const querySnapshot = await getDocs(collection(db, "registrations"));
 
-    querySnapshot.forEach((doc) => {
-        const data = doc.data();
+    querySnapshot.forEach((item) => {
+
+        const data = item.data();
 
         table.innerHTML += `
         <tr>
@@ -21,10 +25,34 @@ async function loadRegistrations() {
             <td>${data.captainName || ""}</td>
             <td>${data.mobile || ""}</td>
             <td>${data.status || "Pending"}</td>
-            <td>View</td>
+
+            <td>
+              <button onclick="updateStatus('${item.id}','Approved')">
+                ✅ Approve
+              </button>
+
+              <button onclick="updateStatus('${item.id}','Rejected')">
+                ❌ Reject
+              </button>
+            </td>
         </tr>
         `;
     });
 }
+
+
+window.updateStatus = async function(id, status){
+
+    const ref = doc(db, "registrations", id);
+
+    await updateDoc(ref, {
+        status: status
+    });
+
+    alert("Status Updated: " + status);
+
+    loadRegistrations();
+}
+
 
 loadRegistrations();
