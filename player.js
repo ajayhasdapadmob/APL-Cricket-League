5b087d86d5a06e72227c615c6fd3b13dbbb6ea12
ajayhasdapadmob <ajayhasda623@gmail.com>
2026-07-22 18:50:35 +0530

    update player.js

--- player.js
+++ player.js
@@ -13,7 +13,7 @@ document.getElementById("playerForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
     try {
-
+alert("Submit Clicked");
         let teamId = localStorage.getItem("teamId");
 
 
@@ -25,44 +25,40 @@ document.getElementById("playerForm").addEventListener("submit", async (e) => {
 
         }
 
-
+console.log("Saving to Firestore...");
         await addDoc(collection(db, "players"), {
 
-            teamId: teamId,
+    teamId: teamId,
 
-            captainName: document.getElementById("captainName").value,
+    captainName: document.getElementById("captainName").value,
 
-            player2: document.getElementById("player2").value,
-            player3: document.getElementById("player3").value,
-            player4: document.getElementById("player4").value,
-            player5: document.getElementById("player5").value,
-            player6: document.getElementById("player6").value,
-            player7: document.getElementById("player7").value,
-            player8: document.getElementById("player8").value,
-            player9: document.getElementById("player9").value,
-            player10: document.getElementById("player10").value,
-            player11: document.getElementById("player11").value,
+    player2: document.getElementById("player2").value,
+    player3: document.getElementById("player3").value,
+    player4: document.getElementById("player4").value,
+    player5: document.getElementById("player5").value,
+    player6: document.getElementById("player6").value,
+    player7: document.getElementById("player7").value,
+    player8: document.getElementById("player8").value,
+    player9: document.getElementById("player9").value,
+    player10: document.getElementById("player10").value,
+    player11: document.getElementById("player11").value,
+    player12: document.getElementById("player12").value,
+    player13: document.getElementById("player13").value,
+    player14: document.getElementById("player14").value,
+    player15: document.getElementById("player15").value,
 
-            player12: document.getElementById("player12").value,
-            player13: document.getElementById("player13").value,
-            player14: document.getElementById("player14").value,
-            player15: document.getElementById("player15").value,
+    createdAt: serverTimestamp()
+});
 
-            createdAt: serverTimestamp()
-        });
-
-
-        alert("✅ Players Saved Successfully!");
+alert("Data Saved");
+alert("✅ Players Saved Successfully!");
 
 console.log("Redirecting...");
 window.location.replace("registration.html");
 
-    } catch(error){
-
-        console.error(error);
-
-        alert("❌ Error: " + error.message);
-
-    }
+} catch (error) {
+    console.error("Firestore Error:", error);
+    alert("Error: " + error.message);
+}
 
 });
